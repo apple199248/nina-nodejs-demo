6b6aa4cc472642b4344c1625a523a722cc4bc423
Nina <liujj@citycloud@org.cn>
2017-08-17 09:26:28 +0800

    新内容

--- eventEmitter.js
+++ eventEmitter.js
@@ -1,10 +1,10 @@
 var EventEmitter = require('events').EventEmitter;
 var event = new EventEmitter();
 
-event.on('some_event',function(){
-	console.log('some_event is runing');
+event.on('some_event',function(arg1){
+	console.log('some_event is runing',arg1);
 })
 
 setTimeout(function(){
-	event.emit('some_event');
+	event.emit('some_event',"这是参数");
 },1000);
